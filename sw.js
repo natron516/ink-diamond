14ac5e1c7aff15c5ea859df0d990d96157d8e52d
notclaw <70186611+natron516@users.noreply.github.com>
2026-09-21 21:37:43 -0700

    Update SW precache for chunked assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const SHELL='ink-diamond-shell-v3';
-const PRECACHE=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/index-D10yon7j.css", "./assets/index-4KBIdLpm.js"];
+const PRECACHE=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/index-D10yon7j.css", "./assets/boot.js", "./assets/js-part-0.txt", "./assets/js-part-1.txt", "./assets/js-part-2.txt", "./assets/js-part-3.txt"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(SHELL).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
