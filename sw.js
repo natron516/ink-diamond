5c08f064f38eb16ae05b79e8b98665280a4347bb
notclaw <70186611+natron516@users.noreply.github.com>
2026-09-21 21:38:46 -0700

    Precache base64 JS chunks in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const SHELL='ink-diamond-shell-v3';
-const PRECACHE=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/index-D10yon7j.css", "./assets/index-4KBIdLpm.js"];
+const PRECACHE=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./assets/index-D10yon7j.css", "./assets/boot.js", "./assets/b64-part-0.txt", "./assets/b64-part-1.txt", "./assets/b64-part-2.txt", "./assets/b64-part-3.txt", "./assets/b64-part-4.txt", "./assets/b64-part-5.txt"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(SHELL).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
